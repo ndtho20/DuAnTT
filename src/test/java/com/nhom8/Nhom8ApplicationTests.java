cb46af6b2dc84f59bbbd178325767d65536e80f2
Dung Tran <108310431+dungttmph25136@users.noreply.github.com>
2023-10-08 22:42:41 +0700

    update name package

--- src/test/java/com/nhom8/Nhom8ApplicationTests.java
+++ src/test/java/com/nhom8/Nhom8ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.nhom8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class Nhom8ApplicationTests {
 
     @Test
     void contextLoads() {
